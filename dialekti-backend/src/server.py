aabb3155750c551daaeeebd76d840123d23642f9
Adem Odza <62858601+AdemOdza@users.noreply.github.com>
2025-02-07 23:27:04 -0600

    Initialize Back-End (#7)

---
+++ dialekti-backend/src/server.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import os
+
+flaskPort = int(os.environ['FLASK_PORT']) if os.environ['FLASK_PORT'] else 3000
+
+app = Flask(__name__)
+app.config['APPLICATION_ROOT'] = '.'
+
+@app.route('/')
+def version():
+    return 'Hello, world!'
+
+@app.route('/version')
+def version():
+    return 'Dialekti v0.1.0'
+
+if __name__ == '__main__':
+      app.run(host='0.0.0.0', port=flaskPort)
